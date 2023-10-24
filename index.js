05196355504ac453b431ec7f1c3f303093bdd10a
spacetourer1983 <mail@deenar.ru>
2023-10-24 14:36:59 +0300

    fix mobile menu

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let menuBtn = document.querySelector(".burger-button");
-let navbar = document.querySelector(".navbar");
-menuBtn.addEventListener("click", function () {
-  navbar.classList.toggle("active");
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".burger-button");
+const closeIcon = document.querySelector(".burger-button__close");
+const showIcon = document.querySelector(".burger-button__show");
+const menuItems = document.querySelectorAll(".navbar-menu__item"); 
+function toggleMenu() {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+    closeIcon.style.display = "none";
+    showIcon.style.display = "block";
+  } else {
+    navbar.classList.add("active");
+    closeIcon.style.display = "block";
+    showIcon.style.display = "none";
+  }
+}
+menuItems.forEach(function(menuItem) {
+  menuItem.addEventListener("click", function() {
+    document.querySelector(".navbar").classList.remove("active");
+  });
 });
+hamburger.addEventListener("click", toggleMenu);
